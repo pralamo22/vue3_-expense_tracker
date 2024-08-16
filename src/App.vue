2e56cf6633811a6a974fbf80adb962f8966d7708
pralamo <pralamo@.pruebas.es>
2024-08-16 07:59:38 +0200

    Paso 3. Empezamos con reactividad. script setup. Minuto 30 del video https://www.youtube.com/watch?v=hNPwdOZ3qFU

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,35 @@
   <div class="container">
     <Balance />
     <IncomeExpenses />
+    <TransactionList />
     <AddTransaction />
+
   </div>
 </template>
 
-<script>
+<script setup>
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
   import IncomeExpenses from './components/IncomeExpenses.vue';
   import TransactionList from './components/TransactionList.vue';
   import AddTransaction from './components/AddTransaction.vue';
-
-  export default {
-    components: {
-      Header,
-      Balance,
-      IncomeExpenses,
-      TransactionList,
-      AddTransaction
-    },
-  };
 </script>
 
+
+<script>
+  // Comentado el aspecto del código para el Options API. Ahora, con 
+  // setup, utilizamos el Composition API.
+  // export default {
+  //   components: {
+  //     Header,
+  //     Balance,
+  //     IncomeExpenses,
+  //     TransactionList,
+  //     AddTransaction
+  //   },
+  // };
+</script>
+<!-- hola -->
 <style lang="css" scoped>
 
 </style>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -3,6 +3,15 @@
   <ul id="list" class="list">
     <li
       v-for="transaction in transactions"
+      v-bind:key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>{{ transaction.amount }} €</span>
+      <button class="delete-btn">x</button>
+    </li>
+
+    <!-- <li
+      v-for="transaction in transactions"
       :key="transaction.id"
       :class="transaction.amount < 0 ? 'minus' : 'plus'"
     >
@@ -10,10 +19,33 @@
       ><button class="delete-btn" @click="deleteTransaction(transaction.id)">
         x
       </button>
-    </li>
+    </li> -->
   </ul>
 </template>
 
-<script setup></script>
+<script setup>
+const transactions = [
+  { id: 1, text: "Flower", amount: -19.99 },
+  { id: 1, text: "Salario", amount: 299.97 },
+  { id: 1, text: "Book", amount: -10 },
+  { id: 1, text: "Camara", amount: -150 },
+];
+
+// Comentado el aspecto del código para el Options API. Ahora, con
+// setup, utilizamos el Composition API.
+// export default {
+//   data() {
+//     return {
+//       transactions: [
+//         { id: 1, text: "Flower", amount: -19.99 },
+//         { id: 1, text: "Salario", amount: 299.97 },
+//         { id: 1, text: "Book", amount: -10 },
+//         { id: 1, text: "Camara", amount: -150 },
+//       ],
+//     };
+//   },
+//   methods: {},
+// };
+</script>
 
 <style lang="css" scoped></style>
